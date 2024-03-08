7c18eaf6e6d728851bccaa814665ce07d0fb4567
sidanova <canhit.77@gmail.com>
2024-03-09 00:26:03 +0700

    Test git first time

---
+++ Assets/Scripts/FSPlay/KiemVu/Network/Data/SDKProtobuf/LoginRep.cs
@@ -0,0 +1,53 @@
+﻿using ProtoBuf;
+
+namespace FSPlay.GameEngine.SDK.ProtoModel
+{
+    /// <summary>
+    /// Đối tượng phản hồi từ SDK về login
+    /// </summary>
+    [ProtoContract]
+    public class LoginRep
+    {
+        /// <summary>
+        /// Mã lỗi
+        /// </summary>
+        [ProtoMember(1)]
+        public bool status = true;
+
+        /// <summary>
+        /// Mô tả
+        /// </summary>
+        [ProtoMember(2)]
+        public string msg = "";
+
+        /// <summary>
+        /// User ID
+        /// </summary>
+        [ProtoMember(3)]
+        public string user_id = "";
+
+        /// <summary>
+        /// User Name
+        /// </summary>
+        [ProtoMember(4)]
+        public string user_name = "";
+
+        /// <summary>
+        /// Thời gian đăng nhập
+        /// </summary>
+        [ProtoMember(5)]
+        public long lTime = 0;
+
+        /// <summary>
+        /// WTF
+        /// </summary>
+        [ProtoMember(6)]
+        public string strCM = "1";
+
+        /// <summary>
+        /// Chuỗi Token
+        /// </summary>
+        [ProtoMember(7)]
+        public string strToken = "";
+    }
+}
---
+++ Assets/Scripts/FSPlay/KiemVu/Network/Data/SDKProtobuf/RegisterRep.cs
@@ -0,0 +1,23 @@
+﻿using ProtoBuf;
+
+namespace FSPlay.GameEngine.SDK.ProtoModel
+{
+    /// <summary>
+    /// Đối tượng phản hồi từ SDK đăng ký
+    /// </summary>
+    [ProtoContract]
+    public class RegisterRep
+    {
+        /// <summary>
+        /// Mã lỗi
+        /// </summary>
+        [ProtoMember(1)]
+        public bool status = true;
+
+        /// <summary>
+        /// Mô tả lỗi
+        /// </summary>
+        [ProtoMember(2)]
+        public string msg = "";
+    }
+}
